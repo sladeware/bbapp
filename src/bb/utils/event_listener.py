661c6cd68e87d2cc80ce31ab21ffaa88fcfbaaee
Oleksandr Sviridenko <oleks.sviridenko@gmail.com>
2013-01-23 09:24:57 +0200

    Move testing to utils. Add logging utils. Remove obsolete modules.

--- src/bb/utils/event_listener.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-
-class EventListener(object):
-  pass
---
+++ src/bb/utils/logging.py
@@ -0,0 +1,7 @@
+# http://www.bionicbunny.org/
+
+from __future__ import absolute_import
+
+from logging import *
+
+get_logger = getLogger
