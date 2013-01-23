93fb9cb48f16dbbf6c97787bd2db6f14f35d954d
Oleksandr Sviridenko <oleks.sviridenko@gmail.com>
2013-01-23 09:24:07 +0200

    Few updates.

--- src/bb/__init__.py
+++ src/bb/__init__.py
@@ -1,7 +1,12 @@
+# http://www.bionicbunny.org/
 # Copyright (c) 2013 Sladeware LLC
+#
+# Author: Oleksandr Sviridenko
 
 from bb import app
 from bb import config
+
+__copyright__ = "Copyright (c) 2013 Sladeware LLC"
 
 bbapp = app
 user_config = config.get_user_config()
